f03c08114be7f522703ceb86ca9e07fe9b2dec80
Кузьмин Алексей Евгеньевич <KuzminAE@sibintek.ru>
2026-03-15 20:31:42 +0300

    Просмотр кода ответа, обработка редиректа

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,8 +2,6 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class HelloWorldTest {
 
