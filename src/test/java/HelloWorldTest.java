fd09aa471a0766f18b434c77a9bd98bfd7a5672f
Кузьмин Алексей Евгеньевич <KuzminAE@sibintek.ru>
2026-03-15 22:29:16 +0300

    просмотр и сохранение куки

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -11,21 +11,27 @@ public class HelloWorldTest {
 
     @Test
     public void testRestAssured() {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("myHeader1", "myValue1");
-        headers.put("myHeader2", "myValue2");
+        Map<String, String> data = new HashMap<>();
+        data.put("login", "secret_login");
+        data.put("password", "secret_pass");
 
         Response response = RestAssured
                 .given()
-                .headers(headers)
+                .body(data)
                 .when()
-                .get("https://playground.learnqa.ru/api/show_all_headers")
+                .post("https://playground.learnqa.ru/api/get_auth_cookie")
                 .andReturn();
 
+        System.out.println("\nPretty text: ");
         response.prettyPrint();
 
+        System.out.println("\nHeaders:");
         Headers responseHeaders = response.getHeaders();
-
         System.out.println(responseHeaders);
+
+        System.out.println("\nCookies:");
+        Map<String, String> responseCookies = response.getCookies();
+        System.out.println(responseCookies);
+
     }
 }
