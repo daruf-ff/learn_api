f4478e35ce077abbe82084cd3e8070f90435d791
Кузьмин Алексей Евгеньевич <KuzminAE@sibintek.ru>
2026-03-15 20:19:01 +0300

    Добавление в body тела через билдер

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld() {
-        System.out.println("Hello World");
+    public void testRestAssured() {
+        Map<String, String> body = new HashMap<>();
+        body.put("param1", "value1");
+        body.put("param2", "value2");
+
+        Response response = RestAssured
+                .given()
+                .body(body)
+                .post("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+
+        //int statusCode = response.getStatusCode();
+        System.out.println(body);
+        response.print();
     }
 }
