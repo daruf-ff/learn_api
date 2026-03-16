87157f86e9eda26dff0cb0f1d65dc421ce8c9170
Кузьмин Алексей Евгеньевич <KuzminAE@sibintek.ru>
2026-03-16 14:06:34 +0300

    просмотр и сохранение куки, переиспользование переменной для другого запроса

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -15,23 +15,32 @@ public class HelloWorldTest {
         data.put("login", "secret_login");
         data.put("password", "secret_pass");
 
-        Response response = RestAssured
+        Response responseForGet = RestAssured
                 .given()
                 .body(data)
                 .when()
                 .post("https://playground.learnqa.ru/api/get_auth_cookie")
                 .andReturn();
 
-        System.out.println("\nPretty text: ");
-        response.prettyPrint();
 
-        System.out.println("\nHeaders:");
-        Headers responseHeaders = response.getHeaders();
-        System.out.println(responseHeaders);
+        String responseCookie = responseForGet.getCookie("auth_cookie");
 
-        System.out.println("\nCookies:");
-        Map<String, String> responseCookies = response.getCookies();
-        System.out.println(responseCookies);
+        Map<String, String> cookies = new HashMap<>();
+        if (responseCookie != null) {
+            cookies.put("auth_cookie", responseCookie);
+
+        }
+
+        Response responseForCheck = RestAssured
+                .given()
+                .body(data)
+                .cookies(cookies)
+                .when()
+                .post("https://playground.learnqa.ru/api/check_auth_cookie")
+                .andReturn();
+
+        responseForCheck.print();
+
 
     }
 }
